a711211a5b7cd7bfc650a80c68ad7d491adcb7e0
Kl3bY <caiovitorruas5@gmail.com>
2023-08-08 21:47:46 -0300

    jorge ui

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,14 +1,27 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<Filmes> listaFilmes;
+
+    //Declarar o recycler view
+    RecyclerView idRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idRecyclerView = findViewById(R.id.idListaFilmes);
+
+        listaFilmes = new ArrayList<>();
     }
 }
