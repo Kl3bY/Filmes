bbee7f21c550e39cfbfe758effc2e173648dd6fc
Kl3bY <caiovitorruas5@gmail.com>
2023-08-08 21:31:08 -0300

    jorge ui

--- app/src/main/java/br/com/etecia/myapp/MyAdapter.java
+++ app/src/main/java/br/com/etecia/myapp/MyAdapter.java
@@ -1,4 +1,59 @@
 package br.com.etecia.myapp;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private Context mContexto;
+    private List<Filmes> lstFilmes;
+
+    public MyAdapter(Context mContexto, List<Filmes> lstFilmes) {
+        this.mContexto = mContexto;
+        this.lstFilmes = lstFilmes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView idTituloFilmes;
+
+        ImageView idImagemFilmes;
+
+        CardView idCardFilmes;
+
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
 }
