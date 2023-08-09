3a3af46cb5dbc448d060d5f4f7bbdad2a640b641
Kl3bY <caiovitorruas5@gmail.com>
2023-08-08 22:08:37 -0300

    jorge ui

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -25,6 +25,48 @@ public class MainActivity extends AppCompatActivity {
 
         listaFilmes = new ArrayList<>();
 
+        listaFilmes.add(
+                new Filmes("O Rei da Montanha",
+                        "spoiler: o rei é da montanha",
+                        "aventura/infantil",
+                        R.drawable.rei_montanha));
+
+        listaFilmes.add(
+                new Filmes("Na Ponta da bala",
+                        "spoiler: a bala ta na ponta",
+                        "ação",
+                        R.drawable.pontabala));
+
+        listaFilmes.add(
+                new Filmes("A Menina que Matou os Pais",
+                        "spoiler: a menina matou os pais",
+                        "documentário/terror",
+                        R.drawable.menina));
+
+        listaFilmes.add(
+                new Filmes("Medo Profundo",
+                        "spoiler: o medo é profundo",
+                        "terror/oceano",
+                        R.drawable.medo));
+
+        listaFilmes.add(
+                new Filmes("2067",
+                        "spoiler: se passa em 2067",
+                        "ficção",
+                        R.drawable.doismilesessetnaesete));
+
+        listaFilmes.add(
+                new Filmes("O Rei da Montanha",
+                        "spoiler: o rei é da montanha",
+                        "aventura/infantil",
+                        R.drawable.rei_montanha));
+
+        listaFilmes.add(
+                new Filmes("O Rei da Montanha",
+                        "spoiler: o rei é da montanha",
+                        "aventura/infantil",
+                        R.drawable.rei_montanha));
+
         MyAdapter adapter = new MyAdapter(getApplicationContext(),listaFilmes);
 
         idRecyclerView.setLayoutManager(new
